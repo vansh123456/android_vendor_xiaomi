fb18662e9e39c6cb5db723442b9580982eee4d01
Vansh <vanshbhatnagar13@gmail.com>
2021-04-27 16:04:16 +0530

    more popup ounds

--- davinci/davinci-vendor.mk
+++ davinci/davinci-vendor.mk
@@ -8,6 +8,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_cangmen_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_cangmen_down.ogg \
     vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_cangmen_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_cangmen_up.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_jojo_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_jojo_down.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_jojo_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_jojo_up.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_anime_moan_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_anime_moan_down.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_anime_moan_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_anime_moan_up.ogg \
     vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_chilun_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_chilun_down.ogg \
     vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_chilun_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_chilun_up.ogg \
     vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_jijia_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_jijia_down.ogg \
